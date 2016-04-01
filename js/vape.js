97085f83d9dc9fb0cf8544415dce61f9d4439bb3
Thomas Maffia <ta.maffia@gmail.com>
2016-03-31 22:48:56 -0400

    add simple angular setup

--- js/vape.js
+++ js/vape.js
@@ -1,13 +1,21 @@
-$(document).ready(function() {
-	$('.record-vape').click(function(){
-		$.get("api/vape", function() {
-			location.reload();
+(function() {
+	var app = angular.module('vape', []);
+	app.controller('RecordCountController', function($scope, $http) {
+		$http.get('api/vapes')
+			.then(function(response) {
+				$scope.count = response.data.Count;
 			});
 	});
 
-	$('.delete-last-record').click(function() {
-		$.get("api/delete-vape", function() {
-			location.reload();
-			});
+	$(document).ready(function() {
+		$('.record-vape').click(function(){
+			$.get("api/vape", function() {
+				});
+		});
+
+		$('.delete-last-record').click(function() {
+			$.get("api/delete-vape", function() {
+				});
+		});
 	});
-});
+ })();
